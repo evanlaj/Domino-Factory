9dc2d2989670f8918a79a1bf445e0f31f28d21e6
Lilian Barbier <lilian.barbierlaffitte@gmail.com>
2024-01-02 10:48:49 +0100

    updated player input management

--- Assets/Scripts/Menu/ButtonBobbing.cs
+++ Assets/Scripts/Menu/ButtonBobbing.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class ButtonBobbing : MonoBehaviour
 {
+    float initialY;
+    RectTransform rectTransform;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        initialY = rectTransform.anchoredPosition.y;
+    }
 
     void Update()
     {
-        RectTransform rectTransform = GetComponent<RectTransform>();
-        rectTransform.anchoredPosition = new Vector3(rectTransform.anchoredPosition.x, Mathf.Sin(Time.time * 4f) * 10f - 3f, 0);
+        rectTransform.anchoredPosition = new Vector3(rectTransform.anchoredPosition.x, initialY + Mathf.Sin(Time.time * 4f) * 10f, 0);
     }
 }
